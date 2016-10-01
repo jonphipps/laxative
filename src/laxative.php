dfe0164e67d71e6155a0caa51cf7436bcce84376
Jon Phipps <jphipps@madcreek.com>
2016-10-01 17:39:24 -0400

    rework laxative to support mysql, and environment-specific backups reconfigures itself on-the-fly to backup the database before and after a suite run in an environment. The database backup from any other environment can be specified in the environment configuration as the source of data for the current environment. This allows a cascading build of database dependencies as the tests are run, as well as allowing tests to be started at any point in the cascade.

--- src/laxative.php
+++ src/laxative.php
@@ -2,6 +2,9 @@
 
 namespace Codeception\Extension;
 
+use Codeception\Event\SuiteEvent;
+use Codeception\Platform\Extension;
+
 /**
  *
  * Fiber, for your dump.
@@ -10,188 +13,323 @@ namespace Codeception\Extension;
  *  - Benjamin Franklin (probably)
  *
  */
-class Laxative extends \Codeception\Platform\Extension
+class Laxative extends Extension
 {
-	// listen to these events
-	public static $events = array(
-		'suite.before' => 'beforeSuite',
-		'suite.after' => 'afterSuite',
-	);
 
-	private $_backup;
-	private $_backupPath;
+  // listen to these events
+  public static $events = [
+      'suite.before' => 'beforeSuite',
+      'suite.after'  => 'afterSuite',
+
+  ];
+
+  private $_backupBefore;
+  private $_backupAfter;
+  private $_backupPath;
+  private $_currentEnvironment;
   private $_migrations;
   private $_seed;
-	private $_host;
-	private $_login;
-	private $_database;
+  private $_host;
+  private $_port;
+  private $_login;
+  private $_database;
+  private $_dumpDatabase;
+  private $_databaseType;
+  private $_user;
+  private $_password;
+  // the Codeception Db module
+  /** @var  \Codeception\Module\Db */
+  private $_db;
 
-	// the Codeception Db module
-	private $_db;
 
-	public function beforeSuite(\Codeception\Event\SuiteEvent $e)
-	{
-		// read config
-		$this->_backup = $this->config['backup'];
-		$this->_backupPath = $this->config['backup_path'];
-    $this->_migrations = $this->config['migrations'];
-    $this->_seed = $this->config['seed'];
-		$this->_host = $this->config['host'];
-		$this->_login = $this->config['login'];
-		$this->_database = $this->config['database'];
+  public function beforeSuite(SuiteEvent $e)
+  {
+    // read config
+    $this->setParams($e);
 
-		// get the db module
-    try {
-      $this->_db = $this->getModule('Db');
-    } catch (\Exception $e) {
-      return;
+    // If enabled, create a mysql|pgsql_dump backup of the current local database
+    // from mysql|pgsql to '_backupPath'.sql
+    if ($this->_backupBefore) {
+      $this->backup($this->_backupPath . '_before' . $this->_currentEnvironment . '.sql');
     }
 
-		// back up local database if enabled
-		if ($this->_backup) {
-			$this->backup();
-		}
+    // Let Codeception restore the database from '_masterDbPath'.sql
+    // by reinitializing the db module, which loads the database
+    $this->localRestore();
 
-		// start from scratch
-		$this->localRestore();
+    // Run all migrations against the freshly restored database.
+    $this->migrate();
 
-		// run migrations
-		$this->migrate();
+    // Run seeder(s) against the freshly restored database.
+    $this->seed();
 
-		// run seeder(s)
-		$this->seed();
+    // Create a new dump file ($this->_db->_getConfig('dump')) from the updated database
+    // for the Codeception Db module to restore for each of the tests in this suite.
+    $dump = 'tests/_data/dump.sql';
+    $this->dump($dump);
 
-		// create Codeception dump
-		$this->dump();
-
-		// update Codeception to populate the database
-		$this->updateDbModule();
-	}
-
-	public function afterSuite(\Codeception\Event\SuiteEvent $e)
-	{
-		// read config
-		$this->_backup = $this->config['backup'];
-		$this->_backupPath = $this->config['backup_path'];
-
-		if ($this->_backup) {
-			$this->restore();
-		}
-	}
-
-	/**
-	 * Create a binary back up of the local database.
-	 */
-	private function backup()
-	{
-		$this->writeln('Laxative: Backing up your local database to: ' . $this->_backupPath . '...');
-
-		// use pg_dump to create binary backup
-		$command = sprintf('pg_dump -h %s -U %s -d %s -F t --file %s',
-			$this->_host,
-			$this->_login,
-			$this->_database,
-			$this->_backupPath);
-
-		exec($command);
-
-		$this->writeln('Done.');
+    // update Codeception to populate the database
+    $this->updateDbModule($dump);
+  }
 
 
-	}
+  public function afterSuite(SuiteEvent $e)
+  {
+    $this->setParams($e);
 
-	/**
-	 * Let Codeception restore the database from base.sql
-	 */
-	private function localRestore()
-	{
-		$this->writeln('Laxative: Restoring local database from base...');
+    if ($this->_backupAfter) {
+      $this->backup($this->_backupPath . '_after' . $this->_currentEnvironment . '.sql');
+    }
 
-		$this->_db->_reconfigure(array('populate' => true));
-		$this->_db->_initialize();
+    if ($this->_backupBefore && $this->_db->_getConfig('cleanup')) {
+      $this->restore($this->_backupPath . '_before' . $this->_currentEnvironment . '.sql');
+    }
+  }
 
-		$this->writeln('Done.');
-	}
 
-	/**
-	 * Restore local database from a binary backup.
-	 * @see backup()
-	 */
-	private function restore()
-	{
-		// get the db module - not needed but will prevent restore for
-		// suites that don't use Db
-	    try {
-	      $this->_db = $this->getModule('Db');
-	    } catch (\Exception $e) {
-	      return;
-	    }
+  /**
+   * Create a binary back up of the local database.
+   *
+   * @param string $destination
+   */
+  private function backup($destination)
+  {
+    $this->writeln('Laxative: Backing up your local database to: ' . $this->_backupPath . '...');
 
-		$this->writeln('Laxative: Restoring your database from backup...');
+    $this->dumpCommand($destination);
 
-		// use pg_restore to restore from our binary backup
-		$command = sprintf('pg_restore -h %s -U %s -d %s -c %s',
-			$this->_host,
-			'postgres',
-			$this->_database,
-			$this->_backupPath);
+    $this->writeln('Done.');
 
-		exec($command);
+  }
 
-		$this->writeln('Done.');
-	}
 
-	/**
-	 * Run all migrations against fresh database.
-	 */
-	private function migrate()
-	{
-    if (!empty($this->_migrations)) {
+  /**
+   * Let Codeception restore the database from base.sql
+   */
+  private function localRestore()
+  {
+    $this->writeln('Laxative: Restoring local database from base...');
+
+    $this->_db->_reconfigure([ 'populate' => true, 'dump' => $this->_dumpDatabase ]);
+    $this->_db->_initialize();
+
+    $this->writeln('Done.');
+  }
+
+
+  /**
+   * Restore local database from a binary backup.
+   * @see backup()
+   *
+   * @param $source
+   */
+  private function restore($source)
+  {
+    $this->writeln('Laxative: Restoring your database from backup...');
+    $this->restoreCommand($source);
+
+    $this->writeln('Done.');
+  }
+
+
+  /**
+   * Run all migrations against fresh database.
+   */
+  private function migrate()
+  {
+    if ( ! empty( $this->_migrations )) {
       $this->writeln('Laxative: Running migrations...');
       exec($this->_migrations);
       $this->writeln('Done.');
     }
-	}
+  }
 
-	/**
-	 * Run all seeders against newly created database.
-	 */
-	private function seed()
-	{
-    if (!empty($this->_seed)) {
+
+  /**
+   * Run all seeders against newly created database.
+   */
+  private function seed()
+  {
+    if ( ! empty( $this->_seed )) {
       $this->writeln('Laxative: Seeding database...');
       exec($this->_seed);
       $this->writeln('Done.');
     }
-	}
+  }
 
-	/**
-	 * Create a dump file for the Codeception Db module.
-	 */
-	private function dump()
-	{
-		$this->writeln('Laxative: Creating Codeception dump...');
 
-		$command = sprintf('pg_dump -h %s -U %s -d %s > tests/_data/dump.sql',
-			$this->_host,
-			$this->_login,
-			$this->_database);
+  /**
+   * Create a dump file for the Codeception Db module.
+   *
+   * @param $dump
+   */
+  private function dump($dump)
+  {
+    $this->writeln('Laxative: Creating Codeception dump...');
 
-		exec($command);
+    $this->dumpCommand($dump);
 
-		$this->writeln('Done.');
-	}
+    $this->writeln('Done.');
+  }
 
-	/**
-	 * Re-configure the Db module to ensure we populate from the dump file.
-	 */
-	private function updateDbModule()
-	{
-		$this->writeln('Laxative: Re-configuring Codeception Db module...');
 
-		$this->_db->_reconfigure(array('dump' => 'tests/_data/dump.sql'));
-		$this->_db->_initialize();
+  /**
+   * Re-configure the Db module to ensure we populate from the dump file.
+   *
+   * @param $dump
+   */
+  private function updateDbModule($dump)
+  {
+    $this->writeln('Laxative: Re-configuring Codeception Db module...');
 
-		$this->writeln('Done.');
-	}
+    $this->_db->_reconfigure([ 'dump' => $dump ]);
+    $this->_db->_initialize();
+
+    $this->writeln('Done.');
+  }
+
+
+  private function dumpCommand($destination)
+  {
+    if ($this->getDatabaseTypeIsPostgres()) { // use pg_dump to create binary backup
+      $command = sprintf('pg_dump -h %s -p %s -U %s -d %s -F t --file %s',
+          $this->_host,
+          $this->_port,
+          $this->_login,
+          $this->_database,
+          $destination);
+    } else { ////$cmd = "mysqldump --routines --databases -h {$server_name} -u {$username} -p{$password} {$database_name} > " . BACKUP_PATH . "{$date_string}_{$database_name}.sql";
+      $command = sprintf('mysqldump --routines --databases -h %s -P %s -u %s -p%s %s > %s',
+          $this->_host,
+          $this->_port,
+          $this->_user,
+          $this->_password,
+          $this->_database,
+          $destination);
+    }
+
+    exec($command);
+  }
+
+
+  private function restoreCommand($source)
+  {
+    if ($this->getDatabaseTypeIsPostgres()) { // use pg_restore to restore from our binary backup
+      $command = sprintf('pg_restore -h %s -p %s -U %s -d %s -c %s',
+          $this->_host,
+          $this->_port,
+          'postgres',
+          $this->_database,
+          $source);
+    } else { //"mysql -h {$server_name} -P {port} -u {$username} -p{$password} {$database_name} < $restore_file"
+      $command = sprintf('mysql -h %s -P %s -u %s -p%s %s < %s',
+          $this->_host,
+          $this->_port,
+          $this->_user,
+          $this->_password,
+          $this->_database,
+          $source);
+    }
+
+    exec($command);
+  }
+
+
+  /**
+   * @return bool
+   */
+  private function getDatabaseTypeIsPostgres()
+  {
+    return $this->_databaseType == strtolower('pgsql');
+  }
+
+
+  /**
+   * @param SuiteEvent $e
+   */
+  private function setParams(SuiteEvent $e)
+  {
+    // get the db module
+    try {
+      $this->_db = $this->getModule('Db');
+    }
+    catch (\Exception $e) {
+      return;
+    }
+
+    $dbDsn = $this->_db->_getConfig('dsn');
+
+    if ($dbDsn) {
+      $dbConfig = [];
+      //pgsql:host=localhost;port=5432;dbname=testdb;user=bruce;password=mypass
+      //"mysql:host=%DB_HOST%;port=%DB_PORT_VM%;dbname=%DB_DATABASE%"
+      $dsn                 = explode(":", $dbDsn);
+      $this->_databaseType = $dsn[0];
+      $values              = explode(";", $dsn[1]);
+      foreach ($values as $value) {
+        $t = explode('=', $value);
+        $dbConfig[$t[0]] = $t[1];
+      }
+
+      if ($this->_databaseType === 'mysql') {
+        $this->_host     = $dbConfig['host'];
+        $this->_port     = isset( $dbConfig['port'] ) ? $dbConfig['port'] : null;
+        $this->_database = $dbConfig['dbname'];
+        $this->_user     = $this->_db->_getConfig('user');
+        $this->_password = $this->_db->_getConfig('password');
+      }
+      if ($this->_databaseType === 'pgsql') {
+        $this->_host     = $dbConfig['host'];
+        $this->_port     = isset( $dbConfig['port'] ) ? $dbConfig['port'] : null;
+        $this->_database = $dbConfig['dbname'];
+        $this->_user     = isset( $dbConfig['user'] ) ? $dbConfig['user'] : null;
+        $this->_password = isset( $dbConfig['password'] ) ? $dbConfig['password'] : null;
+      }
+    } else {
+      $this->_host         = $this->getConfig('host');
+      $this->_port         = $this->getConfig('port');
+      $this->_login        = $this->getConfig('login');
+      $this->_database     = $this->getConfig('database');
+      $this->_databaseType = $this->getConfig('database_type');
+      $this->_user         = $this->_db->_getConfig('user');
+      $this->_password     = $this->_db->_getConfig('password');
+    }
+
+    $this->_currentEnvironment = isset( $e->getSettings()['current_environment']) ? '_' . $e->getSettings()['current_environment'] : '';
+    if ($this->_currentEnvironment) {
+      $DbConfig = $this->getCurrentDbConfig($e);
+      $this->_dumpDatabase = $DbConfig['dump'];
+    } else {
+      $this->_dumpDatabase = $this->_db->_getConfig('dump');
+    }
+
+    $this->_backupBefore = $this->getConfig('backupBefore');
+    $this->_backupAfter  = $this->getConfig('backupAfter');
+    $this->_backupPath   = $this->getConfig('backup_path');
+    $this->_migrations   = $this->getConfig('migrations');
+    $this->_seed         = $this->getConfig('seed');
+
+    return;
+  }
+
+
+  private function getConfig($config)
+  {
+    return isset( $this->config[$config] ) ? $this->config[$config] : '';
+  }
+
+
+  private function getCurrentDbConfig(SuiteEvent $e)
+  {
+    $configs = $e->getSettings()['modules']['config'];
+    foreach ((array) $configs as $config) {
+      foreach ((array) $config as $module => $item) {
+        if ($module == 'Db') {
+          return $config[$module];
+        }
+      }
+    }
+
+    return false;
+  }
 }
